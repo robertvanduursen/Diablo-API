95b9a73779aae0e9ee65bd4c2de906f47b5517c5
Robert van Duursen <robertvduursen@gmail.com>
2020-04-22 08:58:42 +0100

    backup commit
    the return seems to be a one-page html
    - attempting to isolate the items based on legendary tag
    - might be useful to also get set items to mix-n-match

--- utils/xpath_scraping.py
+++ utils/xpath_scraping.py
@@ -18,15 +18,21 @@ page = requests.get("https://us.diablo3.com/en/item/ring/#type=legendary")
 tree = html.fromstring(page.content)
 
 _test = '//*[@class="item-details-text"]'
-buyers = tree.xpath(_test)  # + '/text()')
-print(buyers)
-print(len(buyers))
-for x in buyers:
+legendaries = tree.xpath(_test)  # + '/text()')
+total = 0
+for x in legendaries:
     title = x.xpath('h3[1]/a[1]/text()')
-    secondary = x.xpath('div[1]/*[@class="item-effects"]/*[@class="d3-color-ffff8000"]')
+    _type = x.xpath('/*[@class="item-type"]') # /li[1]/span[1]/text()
+    print('_type', _type)
+    # secondary = x.xpath('div[1]/*[@class="item-effects"]/*[@class="d3-color-ffff8000"]')
+    secondary = x.xpath('div[1]/*[@class="item-effects"]/*[.="Secondary"]/../span')
     if secondary:
         print(title)
-        print(secondary[0].text_content())
+        for y in secondary:
+            print(y.text_content())#, '->', y.attrib)
         print()
+        total += 1
 
+print(total)
+# <p class="item-property-category">Secondary</p>
 # todo: could just simply scrape all the items effects, if the amount of intel trumps the effect
