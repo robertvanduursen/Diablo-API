27d7c1530d827516b186f7aead8c55be3409f07d
Robert van Duursen <robertvduursen@gmail.com>
2020-04-22 08:38:23 +0100

    wonderful scraping

--- utils/xpath_scraping.py
+++ utils/xpath_scraping.py
@@ -18,19 +18,15 @@ page = requests.get("https://us.diablo3.com/en/item/ring/#type=legendary")
 tree = html.fromstring(page.content)
 
 _test = '//*[@class="item-details-text"]'
-buyers = tree.xpath(_test)## + '/text()')
+buyers = tree.xpath(_test)  # + '/text()')
 print(buyers)
 print(len(buyers))
 for x in buyers:
-    # print(x)
-    # print(x.xpath('//*[@class="item-effects"]/span[1]/text()'))
-    #[0].attrib
     title = x.xpath('h3[1]/a[1]/text()')
-
-    secondary = x.xpath('div[1]/*[@class="item-effects"]/span[3]')
+    secondary = x.xpath('div[1]/*[@class="item-effects"]/*[@class="d3-color-ffff8000"]')
     if secondary:
         print(title)
-        print(secondary[0].text)
+        print(secondary[0].text_content())
         print()
 
-# todo:
+# todo: could just simply scrape all the items effects, if the amount of intel trumps the effect
