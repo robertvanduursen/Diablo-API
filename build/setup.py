1fe94b3b2fb91cfdc2f7b899b048f8d075480dc2
Robert van Duursen <43824106+robertvanduursen@users.noreply.github.com>
2023-03-31 11:28:56 +0200

    Update setup.py

--- build/setup.py
+++ build/setup.py
@@ -1,7 +1,23 @@
-"""\
-The setup uses pbr for package management.
-See setup.cfg for real configuration
-"""
-import setuptools
+from setuptools import find_packages, setup
 
-setuptools.setup(setup_requires=["pbr", "setuptools"], pbr=True)
+# Specify the custom path to setup.cfg
+setup_cfg_path = 'path/to/setup.cfg'
+
+setup(
+    name='diablo-api-rvd',
+    version='1.0.0',
+    packages=find_packages(),
+    description='Unofficial season build calculator for Blizzards Diablo 3',
+    author='Robert van Duursen',
+
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+    ],
+)
